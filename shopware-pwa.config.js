b37ba7bb8774dcaaba8659de0e479acfbf1a5e3b
finn2212 <stolle.finn@googlemail.com>
2023-05-24 15:31:06 +0900

    corrct api endpoint

--- shopware-pwa.config.js
+++ shopware-pwa.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   shopwareEndpoint: 'https://26485.s15269.creoline.cloud',
-	  shopwareAccessToken: "SWSCCGJXNTJGA2PVWVZFYNB5RQ",
+	  shopwareAccessToken: "SWSCZNPHTKX6VHMYYJK3UZDGRW",
   shopwareApiClient: {
      // optional, allow to override the default settings
      timeout: 5000, // timeout limit in ms
