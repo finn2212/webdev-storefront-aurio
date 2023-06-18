5825690162729b6f2c528be8d789f49b6f23a14c
finn2212 <stolle.finn@googlemail.com>
2023-06-18 13:13:24 +0200

    domains

--- shopware-pwa.config.js
+++ shopware-pwa.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   shopwareEndpoint: 'https://s23511.creoline.cloud',
-	  shopwareAccessToken: "SWSCUHZMWDM2TTLINJFXMKG3TW",
-    shopwareApiClient: {
-     // optional, allow to override the default settings
-     timeout: 5000, // timeout limit in ms
-   },
-   defaultLanguageCode: "de-DE",
- };
+  shopwareAccessToken: "SWSCUHZMWDM2TTLINJFXMKG3TW",
+  shopwareApiClient: {
+    // optional, allow to override the default settings
+    timeout: 5000, // timeout limit in ms
+  },
+  shopwareDomainsAllowList: [
+    "http://localhost:3000",
+    "https://home-5013243924.app-ionos.space"],
+  defaultLanguageCode: "de-DE",
+};
