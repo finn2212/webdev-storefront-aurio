f814c36875e49a1cd37ee470b6cbf83a85720a13
finn2212 <stolle.finn@googlemail.com>
2023-06-26 07:17:07 +0200

    test channel

--- shopware-pwa.config.js
+++ shopware-pwa.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   shopwareEndpoint: 'https://s23511.creoline.cloud',
-  shopwareAccessToken: "SWSCUHZMWDM2TTLINJFXMKG3TW",
+  shopwareAccessToken: "SWSCNURJNGDGZMKWVNDBSE1IDG",
   shopwareDomainsAllowList: [
     'https://home-5013243924.app-ionos.space'
   ],
