3421abc43b36df1fb715b0c76e40a5b5591e8730
finn2212 <stolle.finn@googlemail.com>
2023-06-26 11:04:30 +0200

    host added

--- shopware-pwa.config.js
+++ shopware-pwa.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   shopwareEndpoint: 'https://s23511.creoline.cloud',
   shopwareAccessToken: "SWSCUHZMWDM2TTLINJFXMKG3TW",
+  pwaHost:"https://home-5013243924.app-ionos.space",
   shopwareDomainsAllowList: [
     'https://home-5013243924.app-ionos.space'
   ],
