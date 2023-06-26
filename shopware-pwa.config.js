2a6da4388156a45ccf60650e7c99298ece94a385
finn2212 <stolle.finn@googlemail.com>
2023-06-26 07:04:49 +0200

    domains

--- shopware-pwa.config.js
+++ shopware-pwa.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   shopwareEndpoint: 'https://s23511.creoline.cloud',
   shopwareAccessToken: "SWSCUHZMWDM2TTLINJFXMKG3TW",
-  shopwareApiClient: {
-    // optional, allow to override the default settings
-    timeout: 5000, // timeout limit in ms
-  },
   shopwareDomainsAllowList: [
     'https://home-5013243924.app-ionos.space'
   ],
