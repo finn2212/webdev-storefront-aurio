85fa8efaecc98c3f4ce90705bf858f1bbe53006b
finn2212 <stolle.finn@googlemail.com>
2023-11-04 15:19:37 +0200

    agbs und wiedderuf

---
+++ src/pages/agbs.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="agb-container container top">
+        <h1>Online-AGB www.capellaprint.com</h1>
+        <p>Stand: 2. Mai 2023</p>
+        <h3>Allgemeine Geschäftsbedingungen (AGB)</h3>
+
+        <!-- AGB Sections -->
+        <section v-for="section in agbSections" :key="section.id" class="pt-5">
+            <h5>{{ section.title }}</h5>
+            <div v-html="section.content"></div>
+        </section>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'agbs',
+    data() {
+        return {
+            // AGB sections content will be defined here
+            agbSections: [
+                {
+                    id: 1,
+                    title: '§ 1 Geltungsbereich und Vertragspartner',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Für den Vertrag gelten ausschließlich diese Allgemeinen Geschäftsbedingungen (nachfolgend: AGB). Entgegenstehende, abweichende oder ergänzende Geschäftsbedingungen werden nicht Vertragsbestandteil, auch wenn wir ihnen nicht ausdrücklich widersprechen. Individuelle Vertragsabreden haben Vorrang.</p>
+                        </li>
+                        <li>
+                            <p>Der Vertrag kommt zustande zwischen dem Besteller (nachfolgend: Auftraggeber) und der AURIO GmbH / AURIOPRINT, vertreten durch ihren Geschäftsführer Sebastian Gabriel, Augsburger Straße 9, D-86497 Horgau (nachfolgend: Auftragnehmer).</p>
+                        </li>
+                        <li>
+                            <p>Der Auftraggeber ist Verbraucher, wenn er den Vertrag zu Zwecken abschließt, die überwiegend weder seiner gewerblichen noch seiner selbständigen beruflichen Tätigkeit zugerechnet werden können. Der Auftraggeber ist Unternehmer, wenn er bei Abschluss des Vertrages in Ausübung seiner gewerblichen oder selbständigen beruflichen Tätigkeit handelt.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 2,
+                    title: '§ 2 Angebot und Vertragsschluss',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Die Abbildung bzw. Beschreibung von Waren und Leistungen auf der Website des Auftragnehmers stellt noch kein rechtlich bindendes Angebot, sondern eine Aufforderung zur Abgabe von Angeboten dar. Ein bindendes Angebot (§ 145 BGB) ist erst die Bestellung der Ware oder Leistung durch den Auftraggeber, welche durch Anklicken des Buttons »JETZT KAUFEN« ausgelöst wird.</p>
+                        </li>
+                        <li>
+                            <p>Der Auftragnehmer kann das Angebot innerhalb von zwei Werktagen durch den Versand einer Auftragsbestätigung per E-Mail annehmen.</p>
+                        </li>
+                        <li>
+                            <p>Die unmittelbar nach der Bestellung automatisch per E-Mail versandte Bestätigung des Zugangs der Bestellung stellt noch keine Vertragsannahme dar.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 3,
+                    title: '§ 3 Lieferung',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Sofern nichts anderes vereinbart ist, liefert der Auftragnehmer an die vom Auftraggeber angegebene Lieferadresse.</p>
+                        </li>
+                        <li>
+                            <p>Die Lieferfristen hängen von der einzelnen Bestellung ab und ergeben sich aus den jeweiligen Waren- und Leistungsbeschreibungen.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 4,
+                    title: '§ 4 Preise; Versandkosten; Zollgebühren',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Bestellungen werden zu dem in der Waren- und Leistungsbeschreibung angegebenen Preis ausgeführt. Sämtliche Preise verstehen sich in Euro und enthalten die gesetzliche Umsatzsteuer und sonstige Preisbestandteile. Nicht enthalten sind die Kosten des Versands; diese trägt der Auftraggeber. Eine Information zu Versandkosten erfolgt in der Waren- bzw. Leistungsbeschreibung. Die konkret anfallenden Versandkosten werden bei Aufruf des Warenkorbs und unmittelbar vor Abgabe der Bestellung angezeigt und auf der Rechnung gesondert ausgewiesen.</p>
+                        </li>
+                    <li>
+                            <p>Fallen Zollgebühren an, trägt diese der Auftraggeber.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 5,
+                    title: '§ 5 Eigentumsvorbehalt',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Bestellungen werden zu dem in der Waren- und Leistungsbeschreibung angegebenen Preis ausgeführt. Sämtliche Preise verstehen sich in Euro und enthalten die gesetzliche Umsatzsteuer und sonstige Preisbestandteile. Nicht enthalten sind die Kosten des Versands; diese trägt der Auftraggeber. Eine Information zu Versandkosten erfolgt in der Waren- bzw. Leistungsbeschreibung. Die konkret anfallenden Versandkosten werden bei Aufruf des Warenkorbs und unmittelbar vor Abgabe der Bestellung angezeigt und auf der Rechnung gesondert ausgewiesen.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 6,
+                    title: '§ 6 Zahlung',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Die Zahlung erfolgt nach Wahl des Auftraggebers mittels eines bei Beginn des Bestellvorgangs
+angezeigten Zahlungsmittels. </p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 7,
+                    title: '§ 7 Zurückbehaltung',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Zur Ausübung eines Zurückbehaltungsrechts ist der Auftraggeber nur berechtigt, sofern sein
+fälliger Gegenanspruch auf demselben Vertragsverhältnis beruht. </p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 8,
+                    title: '§ 8 Gewährleistung; Zulieferungen',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Z(1) Ist die Leistung bzw. die gelieferte Ware mangelhaft, gelten die gesetzlichen Gewährleistungsvorschriften. </p>
+                        </li>
+                        <li>
+                            <p>(2) Zulieferungen (insbesondere Datenträger, übertragene Daten) durch den Auftraggeber oder
+durch einen von ihm eingeschalteten Dritten unterliegen, sofern nicht ausdrücklich anders
+vereinbart, keiner Prüfungspflicht seitens des Auftragnehmers. Dies gilt nicht für die technische
+Eignung von Zulieferungen zur ordnungsgemäßen Erfüllung des Auftrags, soweit die
+mangelnde Eignung einem sorgfältig handelnden Auftragnehmer erkennbar werden muss.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 9,
+                    title: '§ 9 Virenschutz; Datensicherung',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>(1) Bei Datenübertragungen hat der Auftraggeber vor Übersendung jeweils dem neuesten
+technischen Stand entsprechende Schutzprogramme für Computerviren einzusetzen.</p>
+                        </li>
+                        <li>
+                            <p>(2) Die Datensicherung obliegt allein dem Auftraggeber. Der Auftragnehmer ist berechtigt,
+eine Kopie anzufertigen.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 10,
+                    title: '§ 10 Datenlöschung; Archivierung; Versicherung',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>(1) Soweit einer Löschung nicht gesetzliche Aufbewahrungsfristen entgegenstehen, werden die
+dem Auftragnehmer zur Durchführung des Auftrags übermittelten und beim Auftragnehmer
+gespeicherten Daten des Auftraggebers nach Vertragserfüllung gelöscht, es sei denn, der
+Auftraggeber vereinbart mit dem Auftragnehmer ihre Archivierung gem. Abs. 2.</p>
+                        </li>
+                        <li>
+                            <p>(2) Die übermittelten Daten, Datenträger und ähnlichen Materialien sowie Zwischenerzeugnisse
+werden, soweit sie keiner gesetzlichen Aufbewahrungsfrist unterliegen, vom Auftragnehmer nur
+nach ausdrücklicher Vereinbarung und gegen besondere Vergütung über den Zeitpunkt der
+Übergabe des Endprodukts an den Auftraggeber oder seinen Erfüllungsgehilfen hinaus
+archiviert. Sollen die vorbezeichneten Materialien versichert werden, so hat dies bei fehlender
+gesonderter Vereinbarung der Auftraggeber selbst zu besorgen.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 11,
+                    title: '§ 11 Rechte Dritter',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>(1) Der Auftraggeber versichert, dass durch seine Auftragsvorgaben, insbesondere durch von
+ihm gelieferte Vorlagen, keine Rechte Dritter, insbesondere Urheber-, Kennzeichen-,
+Persönlichkeitsrechte oder sonstigen Schutzrechte Dritter verletzt werden und/oder gegen
+geltendes Recht verstoßen.</p>
+                        </li>
+                        <li>
+                            <p>(2) Der Auftraggeber stellt insoweit den Auftragnehmer von sämtlichen Ansprüchen Dritter
+einschließlich der erforderlichen Kosten der Rechtsverteidigung und/oder Rechtsverfolgung
+vollumfänglich frei, es sei denn, der Auftraggeber weist nach, dass ihm ein Verschulden nicht
+zur Last fällt und er allen ihm obliegenden Sorgfalts- und Prüfpflichten nachgekommen ist.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 12,
+                    title: '§ 12 Erfüllungsort; Gerichtsstand',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>Ist der Auftraggeber Unternehmer im Sinne von § 14 BGB, juristische Person des öffentlichen
+Rechts oder öffentlich-rechtliches Sondervermögen, ist Erfüllungsort und ausschließlicher
+Gerichtsstand für alle sich aus dem Vertragsverhältnis ergebenden Streitigkeiten der Sitz des
+Auftragnehmers.</p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 13,
+                    title: '§ 13 Alternative Streitbeilegung gemäß Art. 14 Abs. 1 ODR-VO und § 36 VSBG',
+                    content: `
+                    <ol>
+                        <li>
+                            <p>(1) Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit, die
+Sie unter http://ec.europa.eu/consumers/odr/ finden.</p>
+                        </li>
+                        <li>
+                            <p>(2) Zur Teilnahme an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle ist
+der Auftragnehmer nicht verpflichtet und grundsätzlich nicht bereit.</p>
+                        </li>
+                    </ol>`
+                },
+                // More sections go here
+            ]
+        };
+    }
+};
+</script>
+  
+<style scoped>
+.agb-container {
+    /* Styles for your AGB content */
+}
+</style>
+  
---
+++ src/pages/wiederrufsrecht.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="agb-container container top">
+        <h1>AURIOPRINT Widerrufsrecht</h1>
+        <p>Stand: 01.01.2023</p>
+
+        <!-- AGB Sections -->
+        <section v-for="section in wiederrufsrechtSections" :key="section.id" class="pt-5">
+            <h5>{{ section.title }}</h5>
+            <div v-html="section.content"></div>
+        </section>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'wiederrufsrecht',
+    data() {
+        return {
+            // AGB sections content will be defined here
+            wiederrufsrechtSections: [
+                {
+                    id: 1,
+                    title: '',
+                    content: `
+                    <p>Verbraucher haben ein vierzehntägiges Widerrufsrecht. Verbraucher ist jede natürliche Person, die ein Rechtsgeschäft zu Zwecken abschließt, die überwiegend weder ihrer gewerblichen noch ihrer selbständigen beruflichen Tätigkeit zugerechnet werden können (§ 13 BGB).
+                         </p>
+                         <p>Das Widerrufsrecht besteht nicht bei (Fernabsatz-)Verträgen</p>
+                    <ol>
+                        <li>
+                            <p>zur Lieferung von Waren, die nicht vorgefertigt sind und für deren Herstellung eine individuelle Auswahl oder Bestimmung durch den Verbraucher maßgeblich ist oder die eindeutig auf die persönlichen Bedürfnisse des Verbrauchers zugeschnitten sind,
+                                </p>
+                        </li>
+                        <li>
+                            <p>zur Lieferung von Waren, die schnell verderben können oder deren Vertallsdatum schnell überschritten würde,
+                                </p>
+                        </li>
+                        <li>
+                            <p>zur Lieferung von Ton- oder Videoaufnahmen oder Computersoftware in einer versiegelten Packung, wenn die Versiegelung nach der Lieferung entfernt wurde
+                                </p>
+                        </li>
+                        <li>
+                            <p>zur Lieferung von Zeitungen, Zeitschriften oder Illustrierten mit Ausnahme von Abonnement-Verträgen.
+                                </p>
+                        </li>
+                    </ol>`
+                },
+                {
+                    id: 2,
+                    title: 'Widerrufsbelehrung',
+                    content: `
+                    <p>Sie haben das Recht, binnen vierzehn Tagen ohne Angabe von Gründen diesen Vertrag zu widerrufen. Die Widerrufsfrist beträgt vierzehn Tage ab dem Tag, an dem Sie oder ein von Ihnen benannter Dritter, der nicht der Beförderer ist, die letzte Ware in Besitz genommen haben bzw. hat.
+                    </p>
+                    <p>Um Ihr Widerrufsrecht auszuüben, müssen Sie uns (AURIO GmbH, Augsburger Straße 9, 86497 Horgau / Tel. 08294 8601888 / office@aurioprint.com) mittels einer eindeutigen Erklärung (z. B. ein mit der Post versandter Brief oder E-Mail) über Ihren Entschluss, diesen Vertrag zu widerrufen, informieren. Zur Wahrung der Widerrufsfrist reicht es aus, dass Sie die Mitteilung über die Ausübung des Widerrufsrechts vor Ablauf der Widerrufsfrist absenden. Sie können dafür das beigefügte Muster-Widerrufsformular verwenden, das jedoch nicht vorgeschrieben ist.
+                    </p>`
+                },
+                {
+                    id: 3,
+                    title: 'Folgen des Widerrufs',
+                    content: `
+                    <p>Wenn Sie diesen Vertrag widerrufen, haben wir Ihnen alle Zahlungen, die wir von Ihnen erhalten haben, einschließlich der Lieferkosten (mit Ausnahme der zusätzlichen Kosten, die sich daraus ergeben, dass Sie eine andere Art der Lieferung als die von uns angebotene, günstigste Standardlieferung gewählt ha-ben), unverzüglich und spätestens binnen vierzehn Tagen ab dem Tag zurück-zuzahlen, an dem die Mitteilung über Ihren Widerruf dieses Vertrags bei uns eingegangen ist. Für diese Rückzahlung verwenden wir dasselbe Zahlungsmit-tel, das Sie bei der ursprünglichen Trans aktion eingesetzt haben, es sei denn, mit Ihnen wurde ausdrücklich etwas anderes vereinbart; in keinem Fall werden Ihnen wegen dieser Rückzahlung Entgelte berechnet. Wir können die Rückzahlung verweigern, bis wir die Waren wieder zurückerhalten haben oder bis Sie den Nachweis erbracht haben, dass Sie die Waren zurückgesandt haben, je nachdem, welches der frühere Zeitpunkt ist.
+                    </p>
+                    <p>Sie haben die Waren unverzüglich und in jedem Fall spätestens binnen vierzehn Tagen ab dem Tag, an dem Sie uns über den Widerruf dieses Vertrags unterrichten, an uns zurückzusenden oder zu übergeben. Die Frist ist gewahrt, wenn Sie die Waren vor Ablauf der Frist von vierzehn Tagen absenden. Sie tragen die unmittelbaren Kosten der Rücksendung der Waren.
+                    </p>
+                    <p>Sie müssen für einen etwaigen Wertverlust der Waren nur aufkommen, wenn dieser Wertverlust auf einen zur Prüfung der Beschaffenheit, Eigenschaften und Funktionsweise der Waren nicht notwendigen Umgang mit ihnen zurückzuführen ist.
+                    </p>`
+                },
+            ]
+        };
+    }
+};
+</script>
+  
+<style scoped>
+.agb-container {
+    /* Styles for your AGB content */
+}
+</style>
+  
